660c5d9a87639e78488abc92d31b7ecb7cf94d9a
karim <karim-nar@live.fr>
2021-02-09 22:31:35 +0100

    initial commit

---
+++ src/main/java/tn/esprit/dari/DariApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.dari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DariApplication.class, args);
+    }
+
+}
