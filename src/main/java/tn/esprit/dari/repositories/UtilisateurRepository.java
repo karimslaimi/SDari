5107a416b12646d27ca5f95989f6d7ce222f8c1f
beheddine <beheddine.akermi@esprit.tn>
2021-03-03 23:40:35 +0100

    user's entity

--- src/main/java/tn/esprit/dari/repositories/UtilisateurRepository.java
+++ src/main/java/tn/esprit/dari/repositories/UtilisateurRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.dari.entities.Utilisateur;
 
+import java.util.Optional;
+
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur,Long> {
 
--- src/main/java/tn/esprit/dari/repositories/VerificationTokenRepository.java
+++ src/main/java/tn/esprit/dari/repositories/VerificationTokenRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.dari.entities.VerificationToken;
 
+import java.util.Optional;
+
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken,Long> {
 
+    Optional<VerificationToken> findByToken(String token);
 }
