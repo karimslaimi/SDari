0710c52f48386ef7d3a74f34cccbdf8ea7aa2bd4
dorra <dorra.kerro@gmail.com>
2021-03-03 23:45:48 +0100

    relations fixed !! services fixed ! controller almost

--- src/main/java/tn/esprit/dari/controllers/SubscriptionController.java
+++ src/main/java/tn/esprit/dari/controllers/SubscriptionController.java
@@ -1,4 +1,49 @@
 package tn.esprit.dari.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.dari.entities.Subscription;
+import tn.esprit.dari.service.ISubscription;
+
+@Controller
+@RequestMapping("/Subscription")
 public class SubscriptionController {
+
+    @Autowired
+    ISubscription subscription;
+
+    @GetMapping("/GetAll")
+    public void getAll(){
+        subscription.AllSubs();
+    }
+    @GetMapping("/getOne/{id}")
+    public void getSub(String id){
+        subscription.getSub(id);
+    }
+
+
+    @PostMapping("/addsub")
+    @ResponseBody
+    public Subscription Add(@RequestBody Subscription s){
+        Subscription sub = subscription.Add(s);
+        return (sub) ;
+
+    }
+
+    @DeleteMapping("/delete/{id]")
+    @ResponseBody
+    public void DeleteSub(@PathVariable("id") String id){
+        subscription.DeleteSub(id);
+    }
+
+    @PutMapping("/Modify")
+    @ResponseBody
+    public Subscription modify(@RequestBody Subscription su){
+        return subscription.Modify(su);
+    }
+
+
+
 }
