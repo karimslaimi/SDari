a96fb5053b8f8e5ee486fb131eb3945c9e7210c3
dorra <dorra.kerro@gmail.com>
2021-03-04 01:08:29 +0100

    controller done!

--- src/main/java/tn/esprit/dari/controllers/SubscriptionController.java
+++ src/main/java/tn/esprit/dari/controllers/SubscriptionController.java
@@ -2,6 +2,8 @@ package tn.esprit.dari.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.dari.entities.Subscription;
@@ -15,12 +17,16 @@ public class SubscriptionController {
     ISubscription subscription;
 
     @GetMapping("/GetAll")
-    public void getAll(){
-        subscription.AllSubs();
+    public ResponseEntity<?> getAll(){
+        return new ResponseEntity<>(subscription.AllSubs(), HttpStatus.OK);
+
+
     }
+
     @GetMapping("/getOne/{id}")
-    public void getSub(String id){
-        subscription.getSub(id);
+    public ResponseEntity<?> getSub(@PathVariable  int id){
+
+        return new ResponseEntity<>( subscription.getSub(id), HttpStatus.OK);
     }
 
 
@@ -34,7 +40,7 @@ public class SubscriptionController {
 
     @DeleteMapping("/delete/{id]")
     @ResponseBody
-    public void DeleteSub(@PathVariable("id") String id){
+    public void DeleteSub(@PathVariable("id") int id){
         subscription.DeleteSub(id);
     }
 
