9e45d1706aa6b0e89475032596de7efbae355982
dorra <dorra.kerro@gmail.com>
2021-03-10 23:55:30 +0100

    done simulation

--- src/main/java/tn/esprit/dari/service/SimulationService.java
+++ src/main/java/tn/esprit/dari/service/SimulationService.java
@@ -5,25 +5,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SimulationService implements ISimulation{
+
     @Override
-    public double CalculMensualiteSA(double montantCredit, int interet, long duree) {
+    public double CalculMensualiteSA(double montantCredit, float taux_period, long duree) {
+
         double m ;
         double sup;
-        double inf ;
-        sup = (montantCredit * interet) / 12 ;
-        double puissance = Math.pow(1+(interet/12), (duree*12));
-        inf = (1- puissance )  ;
-        m = sup / inf ; 
+
+        double pui = Math.pow( (1+taux_period),(duree * 12)) ;
+        sup = (montantCredit * taux_period) *  pui ;
+        double inf = Math.pow((1+taux_period), ((duree * 12) - 1));
+        m = sup / inf ;
         return m;
     }
 
     @Override
-    public double CalculMensualiteAA(double montantCredit, int interet, long duree, double assurance) {
-        return 0;
+    public double CalculMensualiteAA(double montantCredit, float taux_period, long duree, int assurance) {
+
+        double montant_assurance = (assurance * montantCredit) / 12 ;
+        double ma = CalculMensualiteSA(montantCredit, taux_period, duree) + montant_assurance ;
+        return ma;
     }
 
     @Override
-    public double CreditTotal(double mensualiteAA, double interet, double assurance, double frais) {
-        return 0;
+    public double CreditTotal(double montantCredit, long duree ,float taux_period, double assurance, double frais) {
+
+        double montant_interet =(( CalculMensualiteSA(montantCredit,taux_period,duree) * (duree * 12 ) ) - montantCredit ) ;
+        double montant_assurance = (assurance * montantCredit) / 12 ;
+        double total = CalculMensualiteSA(montantCredit,taux_period,duree) + montant_interet + montant_assurance + frais ;
+        return total;
     }
 }
